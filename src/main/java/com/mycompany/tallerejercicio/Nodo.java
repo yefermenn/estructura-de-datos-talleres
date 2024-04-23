30c4c765cf7196ff7a207c4535d7bdc388fa879c
yefermenn <yefersoncamilo3@gmail.com>
2024-04-23 08:00:50 -0500

    primer commit

---
+++ src/main/java/com/mycompany/tallerejercicio/Nodo.java
@@ -0,0 +1,48 @@
+package com.mycompany.tallerejercicio;
+public class Nodo {
+
+    /**
+     * @return the anterior
+     */
+    public Nodo getAnterior() {
+        return anterior;
+    }
+
+    /**
+     * @param anterior the anterior to set
+     */
+    public void setAnterior(Nodo anterior) {
+        this.anterior = anterior;
+    }
+
+    /**
+     * @return the siguiente
+     */
+    public Nodo getSiguiente() {
+        return siguiente;
+    }
+
+    /**
+     * @param siguiente the siguiente to set
+     */
+    public void setSiguiente(Nodo siguiente) {
+        this.siguiente = siguiente;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    private Nodo anterior;
+    private Nodo siguiente;
+    private String nombre;
+}
